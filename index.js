898995241bf579df97c5106a28db52beeb164cd8
Hamza <ansarihamza919@gmail.com>
2025-05-29 16:42:01 +0530

    fixed cors

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const port = process.env.PORT || 3000
 const cors = require('cors')
 require('./models/index')
 app.use(express.json())
-app.use('/api', require('./routes'))
 app.use(cors({
     origin: ['http://localhost:5173', 'https://learning-portal-frontend.vercel.app']
 }));
+app.use('/api', require('./routes'))
 
 
 
